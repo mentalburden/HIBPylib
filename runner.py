ef3e0b93561e7ce2119e2888bc49ea21a30d8e56
Totale Reelnamme <47120653+mentalburden@users.noreply.github.com>
2021-01-06 21:30:35 -0800

    Update runner.py
    name gen done, needs jsontoarray -> csv, then lambda/app'ification

--- runner.py
+++ runner.py
@@ -8,19 +8,21 @@
 #everything is shoved into a clean array
 #Array to csv, then do latex report gen and nosql posts
 
-#done: api auth, check for plaintext domain, check for full plaintext addy
+#done: check for plaintext domain, check for full plaintext addy, generate first.last and flast users for domain and check all
 
-#needs: check for single/last name and username word list combo addresses for known good breaches, array to csv crap, and lambda compat POST/PUT input and response to API'ify script (WIP)
+#needs: lambda compat POST/PUT input and response to API'ify script (WIP), make jsontoarray -> csv/lambda response happen
 
+import time
 import json
 import requests
 
 apiurl = 'https://haveibeenpwned.com/api/v3'
+apikey = "lolno"
 reportout = ''
 lnamefile = "lnames.txt"
 fnamefile = "fnames.txt"
 
-def jsonchonker(thisjason):
+def jsontoarray(thisjason):
         #grabs all the important stuff and creates a str array
         #shrink this to one liners later on and fix the nonetype error
         try:
@@ -33,18 +35,20 @@ def jsonchonker(thisjason):
         except:
                         return ["nope", "nope", "nope", "nope"]
 
+
+
 def checkdomain(query, apikey):
         apiendpoint = apiurl + '/breaches/?domain=' + query
         headerboi =  {'user-agent': 'MBdevops v1 hibpscript','hibp-api-key': str(apikey)}
         req = requests.get(apiendpoint, headers=headerboi)
-        jason = jsonchonker(req.json())
+        jason = jsontoarray(req.json())
         if jason is not None:
                 #start drilldown func here for good hit
                 print(jason)
         else:
                 print("Nothing found for " + query)
 
-def checkaccount(query, apikey):
+def checkuser(query, apikey):
         cleanquery = query.replace('@',"%40") #gets around using urllib for encoding
         apiendpoint = apiurl + '/breachedaccount/' + cleanquery
         headerboi =  {'user-agent': 'MBdevops v1 hibpscript','hibp-api-key': str(apikey)}
@@ -52,24 +56,35 @@ def checkaccount(query, apikey):
         print(req.content)
 
 def genfirstdotlast(fnames, lnames, domain):
+        #tiffany.chonksmith@whatever.net
+        thisarray = []
         for lname in lnames:
                 for fname in fnames:
-                        print(fname.replace("\n", '') + "." + lname.replace("\n", "") + domain)
+                        if (len(fname) > 2): # remove empty \n index
+                                thisarray.append(fname.replace("\n", '') + "." + lname.replace("\n", "") + domain)
+        return thisarray
+
 
 def genletterdotlast(lnames, domain):
+        #t.chonksmith@whatever.net
+        thisarray = []
         alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         for lname in lnames:
                 for letter in alphabet:
-                        print(letter + lname.replace("\n", '') + domain)
+                        if (len(letter) >= 1): # removes empty \n index
+                                thisarray.append(letter + lname.replace("\n", '') + domain)
+        return thisarray
+
 
 ###MAIN STARTS HERE
-#testing namegen...
-genfirstdotlast(open(fnamefile, 'r').readlines(), open(lnamefile, 'r').readlines(), "@chonkerdomain.net")
-genletterdotlast(open(lnamefile, 'r').readlines(), "@chonkerdomain.net")
+#namegen works and returns a username array for given domain
+#print(genfirstdotlast(open(fnamefile, 'r').readlines(), open(lnamefile, 'r').readlines(), "@chonkerdomain.net"))
 
-#get apikey, make it global later on
-#with open('apikey.txt', 'r') as apifile:
-#       apikey = apifile.read().replace('\n','')
+#namegen -> checkusername call with rate limiter example
+#for user in (genletterdotlast(open(lnamefile, 'r').readlines(), "@adobe.net")):
+#       checkuser(user, apikey)
+#       time.sleep(1.5);
+
 #get target list, make it argparse or lambda'able later on
 #with open('targets.txt', 'r') as targetfile:
 #       targetlist = targetfile.read().splitlines() # \n removal hax
