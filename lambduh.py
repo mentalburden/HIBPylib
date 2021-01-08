7185064029ec7c06ff8fbb343df8669dea674ec4
Totale Reelnamme <47120653+mentalburden@users.noreply.github.com>
2021-01-07 19:38:25 -0800

    Update lambduh.py

--- lambduh.py
+++ lambduh.py
@@ -7,10 +7,35 @@ s3_client = boto3.client('s3')
 webber = urllib3.PoolManager()
 
 apiurl = 'https://haveibeenpwned.com/api/v3'
-apikey = "nope" #MB/ owned api key, good till 6FEB20
+apikey = "noway" #MB/ owned api key, good till 6FEB20
+bucket_name = "hibp-scraper-emilyt"
+s3_fullpath = "testchonkers/"
+
+#stuff for later starts here
 reportout = ''
 lnamefile = "lnames.txt"
 fnamefile = "fnames.txt"
+
+def genfirstdotlast(fnames, lnames, domain):
+        #tiffany.chonksmith@whatever.net
+        thisarray = []
+        for lname in lnames:
+                for fname in fnames:
+                        if (len(fname) > 2): # remove empty \n index
+                                thisarray.append(fname.replace("\n", '') + "." + lname.replace("\n", "") + domain)
+        return thisarray
+
+def genletterdotlast(lnames, domain):
+        #tchonksmith@whatever.net
+        thisarray = []
+        alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        for lname in lnames:
+                for letter in alphabet:
+                        if (len(letter) >= 1): # removes empty \n index
+                                thisarray.append(letter + lname.replace("\n", '') + domain)
+        return thisarray
+
+#hibp to s3/csv starts here
 
 def jsontoarray(thisjason):
         #grabs all the important stuff and creates a str array
@@ -24,8 +49,6 @@ def jsontoarray(thisjason):
                         return [name, mdate, longdesc, pwncount]
         except:
                         return
-
-
 
 def checkdomain(query, apikey):
         apiendpoint = apiurl + '/breaches/?domain=' + query
@@ -45,68 +68,51 @@ def checkuserisbrokenrightnow(query, apikey): #needs to get moved to urllib3 ins
         req = requests.get(apiendpoint, headers=headerboi)
         print(req.content)
 
-def genfirstdotlast(fnames, lnames, domain):
-        #tiffany.chonksmith@whatever.net
-        thisarray = []
-        for lname in lnames:
-                for fname in fnames:
-                        if (len(fname) > 2): # remove empty \n index
-                                thisarray.append(fname.replace("\n", '') + "." + lname.replace("\n", "") + domain)
-        return thisarray
-
-
-def genletterdotlast(lnames, domain):
-        #tchonksmith@whatever.net
-        thisarray = []
-        alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-        for lname in lnames:
-                for letter in alphabet:
-                        if (len(letter) >= 1): # removes empty \n index
-                                thisarray.append(letter + lname.replace("\n", '') + domain)
-        return thisarray
-
-def writetos3(domain, outarray):
-        bucket_name = "hibp-scraper-"
-        try:
-               chonk = ','.join(outarray)
-        except:
-                chonk = "none-found, , , "
-        encoded_string = chonk.encode("utf-8")
-        #lambda_path = "/tmp/" + domain + "-record.csv"
-        s3_path = "testchonkers/" + domain + "-record.csv"
-        s3 = boto3.resource("s3")
-        s3.Bucket(bucket_name).put_object(Key=s3_path, Body=encoded_string)
-        
 def readfroms3(filetoread):
-        bucket_name = "hibp-scraper-emilyt"
         s3 = boto3.resource("s3")
         thisfile = s3.Object(bucket_name, filetoread)
         rawfile = thisfile.get()['Body'].read().decode('utf-8')
         print(rawfile)
-        return rawfile.splitlines()
+        return rawfile
         
 def checkfileexists(domain):
-        bucket_name = "hibp-scraper-"
-        s3_path = "testchonkers/" + domain + "-record.csv" #globalize these later you dumdum
+        s3_path = s3_fullpath + domain + "-record.csv" #globalize these later you dumdum
         try:
                 obj = s3_client.head_object(Bucket=bucket_name, Key=s3_path)
                 return s3_path
         except:
                 return None
+
+def writetos3(domain, newarray, isnew, currentcontent):
+        chonk = ""
+        if isnew is True:
+                for i in newarray:
+                        chonk += str(i) + ", "
+                chonk += "\n"
+        elif isnew is False:
+                chonk = currentcontent + "\n"
+                for i in newarray:
+                        chonk += str(i) + ", "
+                chonk += "\n"
+        s3_path = s3_fullpath + domain + "-record.csv"
+        s3 = boto3.resource("s3")
+        s3.Bucket(bucket_name).put_object(Key=s3_path, Body=str(chonk))
         
 def updates3file(domain, outarray):
         filestate = checkfileexists(domain)
         if filestate is not None:
-                thisfile = readfroms3(filestate)
-                newout = str(thisfile) + str(outarray)
-                writetos3(domain, newout)
+                currentcontent = readfroms3(filestate) #Cast old file read as string for easier \n handling
+                writetos3(domain, outarray, False, currentcontent)
         elif filestate is None:
-                writetos3(domain, outarray)
+                writetos3(domain, outarray, True, "NOPE")
 
 ###main starts here sorta
 def lambda_handler(event, context):
-    targets = readfroms3("targets.txt")
+    targets = readfroms3("targets.txt").splitlines() #array'ify the target list
     for target in targets:
         print(target)
         chonklet = checkdomain(target, apikey)
-        updates3file(str(target), chonklet)
+        if chonklet is not None:
+                updates3file(str(target), chonklet)
+        else:
+                updates3file(str(target), [str(target), "No-Breach-Found", "Datestamp-here", "somethingelse-here"])
