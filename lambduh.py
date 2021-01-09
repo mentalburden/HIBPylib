3be1ac382cdcb3acbe997c0fef6651592ad4dd88
Totale Reelnamme <47120653+mentalburden@users.noreply.github.com>
2021-01-08 17:18:04 -0800

    Update lambduh.py
    works per the intial request made, get them to agree to fire jsons directly into firebase or ES/Hive to generate alerts, plead with mgmt to move to jsons and move away from csv bs, shrink the spaghetti funcs, build an api handle so it can be fired from cronjob instead of lambda trigger

--- lambduh.py
+++ lambduh.py
@@ -6,16 +6,18 @@ import urllib3
 s3_client = boto3.client('s3')
 webber = urllib3.PoolManager()
 
-apilimit = 1.5 #figure out tighter limit later, just set runtime to 10min+
+apilimit = 0.3
 apiurl = 'https://haveibeenpwned.com/api/v3'
-apikey = "" #MB/ owned api key, good till 6FEB20
-bucket_name = "hibp-scraper-emilyt"
-s3_fullpath = "testchonkers/"
+apikey = "" #MB owned api key, good till 6FEB20-------------------------REDACT ME!
+bucket_name = "hibp-scraper-" #-------------------------------------------------------------------------REDACT ME!
+s3_fullpath = "examplechonkers/"
+thistime = time.strftime("%Y-%m-%d %H:%M:%S") #use logging/syslog'er later on
 
-#stuff for later starts here
+#namegen junk starts here
 reportout = ''
 lnamefile = "lnames.txt"
 fnamefile = "fnames.txt"
+testnamefile = "tnames.txt"
 
 def genfirstdotlast(fnames, lnames, domain):
         #tiffany.chonksmith@whatever.net
@@ -29,60 +31,79 @@ def genfirstdotlast(fnames, lnames, domain):
 def genletterdotlast(lnames, domain):
         #tchonksmith@whatever.net
         thisarray = []
-        alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        #alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] #full run
+        alphabet = ['a'] #debug
         for lname in lnames:
                 for letter in alphabet:
                         if (len(letter) >= 1): # removes empty \n index
-                                thisarray.append(letter + lname.replace("\n", '') + domain)
+                                thisarray.append(letter + lname.replace("\n", '') + "@" + domain)
         return thisarray
 
 #hibp to s3/csv starts here
 
-def checkuser(query, apikey): 
-        cleanquery = query.replace('@',"%40") #switch back to urllib encode later
-        apiendpoint = apiurl + '/breachedaccount/' + cleanquery
-        headerboi =  {'user-agent': 'MBdevops v1 hibpscript','hibp-api-key': str(apikey)}
-        req = webber.request('GET', apiendpoint, headers=headerboi)
-        reqjson = json.loads(req.data.decode('utf-8'))
+def userjsontoarray(thisjason):
+        #grabs all the important stuff and creates a str array
+        #shrink this to one liners later on and fix the nonetype error
+        #print(thisjason)
+        userbreachdomains = []
         try:
-                jason = jsontoarray(reqjson)
-                time.sleep(apilimit)
-                return jason
+                for chonk in thisjason:
+                        name = str(chonk['Name'])
+                        userbreachdomains.append(name)
+                return userbreachdomains
         except:
-                time.sleep(apilimit)
-                return [str(query), "no-breaches-found", "no-breaches-found", "no-breaches-found"]
+                        return
 
-def jsontoarray(thisjason):
+def checkuser(query, apikey): 
+        cleanquery = query.replace('@',"%40").lower() #switch back to urllib encode later
+        apiendpoint = apiurl + '/breachedaccount/' + cleanquery
+        headerboi =  {'Content-Type': 'application/json', 'user-agent': 'MBdevops hibpscript-checkuser V1','hibp-api-key': str(apikey)}
+        req = webber.request('GET', apiendpoint, headers=headerboi)
+        #print(str(req.data) + " " + cleanquery + "\n")
+        try:
+                reqjson = json.loads(req.data.decode('utf-8'))
+                jason = userjsontoarray(reqjson)
+                time.sleep(apilimit)
+                return [query.lower(), "BREACHES FOUND", jason, thistime]
+        except Exception as errboi:
+                #time.sleep(apilimit)
+                #return Nonetype later on, this is fine for now doesnt do anything tho
+                #return [query.lower(), "no-breaches-found", "no-breaches-found", thistime]
+                return None
+
+def breachjsontoarray(thisjason):
         #grabs all the important stuff and creates a str array
         #shrink this to one liners later on and fix the nonetype error
         try:
                 for chonk in thisjason:
-                        name = str(chonk['Name'])
-                        mdate = str(chonk['ModifiedDate'])
-                        longdesc = str(chonk['Description'])
-                        pwncount = str(chonk['PwnCount'])
-                        return [name, mdate, longdesc, pwncount]
+                        name = "BREACH NAME: " + str(chonk['Name'])
+                        mdate = "BREACH DATE: " + str(chonk['ModifiedDate'])
+                        longdesc = "LONG DESC: {{{" + str(chonk['Description']) + "}}}"
+                        pwncount = "BREACHED ACCOUNTS: " + str(chonk['PwnCount'])
+                        #and script run timestamp
+                        return [name, mdate, longdesc, pwncount, thistime]
         except:
                         return
 
 def checkdomain(query, apikey):
         apiendpoint = apiurl + '/breaches/?domain=' + query
-        headerboi =  {'user-agent': 'MBdevops v1 hibpscript','hibp-api-key': str(apikey)}
+        headerboi =  {'user-agent': 'MBdevops hibpscript-checkbreach V1','hibp-api-key': str(apikey)}
         req = webber.request('GET', apiendpoint, headers=headerboi)
         reqjson = json.loads(req.data.decode('utf-8'))
         try:
-                jason = jsontoarray(reqjson)
+                jason = breachjsontoarray(reqjson)
                 time.sleep(apilimit)
-                return jason
+                return jason #get them to scrap the csv crap and go json, but later
         except:
-                time.sleep(apilimit)
-                return [str(query), "no-breaches-found", "no-breaches-found", "no-breaches-found"]
+                #time.sleep(apilimit)
+                #return Nonetype later on, this is fine for now doesnt do anything tho
+                return [str(query), "no-breaches-found", "no-breaches-found", "no-breaches-found"] 
 
 def readfroms3(filetoread):
         s3 = boto3.resource("s3")
         thisfile = s3.Object(bucket_name, filetoread)
         rawfile = thisfile.get()['Body'].read().decode('utf-8')
-        print(rawfile)
+        #print(rawfile)
         return rawfile
         
 def checkfileexists(domain):
@@ -114,7 +135,7 @@ def updates3file(domain, outarray):
                 currentcontent = readfroms3(filestate) 
                 writetos3(domain, outarray, False, currentcontent) # stupid bool, change it later dumdum
         elif filestate is None:
-                writetos3(domain, outarray, True, "NOPE")
+                writetos3(domain, outarray, True, "NOAP") #fix placeholder with multivar func later
 
 def targetlistrunner():
         validtargets = []
@@ -126,18 +147,26 @@ def targetlistrunner():
                         updates3file(str(target), chonklet)
                         validtargets.append(target)
                 else:
-                        updates3file(str(target), [str(target), "No-Breach-Found", "Datestamp-here", "somethingelse-here"])
+                        updates3file(str(target), [str(target), "No-Breach-Found", "other info can be added...", thistime])
         return validtargets
         
-def userbrutefirstlast(domain):
-        #blah... needs to be valid breach domain
-        #then spin up username gen (or check for dc/user dump file)
-        # foreach addy checkuser(), spit any valid records into arraybanged csv
-        return "bleh"
+def letterdotlastrunner(domain):
+        breachedusers = []
+        for user in genletterdotlast(readfroms3(fnamefile).splitlines(), domain):
+                thisuser = checkuser(user, apikey)
+                if thisuser is not None:
+                        #FIX THIS CRAP BELOW: DUMB AF
+                        #do a single write/append on breachedusers and rebuild the csv stringbanger for userbreach funcs
+                        userfile = str(domain) + "-userbreaches"
+                        updates3file(userfile, thisuser) 
+                        breachedusers.append(thisuser)
+        return breachedusers #fire this array into ES as syslog form'ed jsons for hive alerts
 
 ###main starts here sorta
 def lambda_handler(event, context):
-        targets = targetlistrunner() #shrink this func chain, too sprawling
+        targets = targetlistrunner() #shrink this func chain, ooo000 spaghet
         print(targets)
         for target in targets:
-                userbrutefirstlast(target)
+                print("running user targets")
+                for user in letterdotlastrunner(target):
+                        print(user)
